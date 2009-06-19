1d1936eaaeeaf459d17e661afbabce9cd3131b19
wenju <liuwenju26@gmail.com>
2009-06-19 15:18:10 +0800

    add css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def body_class
     "#{controller.controller_name} #{controller.controller_name}-#{controller.action_name}"
   end
+  
+  def tab_fu(name, path='', right=false)
+    html = ""   
+    if right
+      html << "<li style='float:right;'>"
+    else
+      html << "<li>"
+    end            
+    
+    html << link_to(name, path)
+    html << "</li>"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
+  map.connect 'logout', :controller => "user_sessions", :action => "destroy"
+  
   map.root :controller => "user_sessions", :action => "new"
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
