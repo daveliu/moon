7900b522f088de4a49607ee6f35df4b903f163d0
wenju <liuwenju26@gmail.com>
2009-06-19 14:16:10 +0800

    add auth

--- config/environment.rb
+++ config/environment.rb
@@ -19,11 +19,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+  config.gem "authlogic"
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
+  config.action_controller.session_store = :active_record_store  
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resource :account, :controller => "users"
+  map.resources :users
+  map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new"
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
