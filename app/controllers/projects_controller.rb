be0e95c50ed94b543275da02657f0bc380820f4c
wenju <liuwenju26@gmail.com>
2009-06-29 18:30:11 +0800

    fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
-  skip_before_filter :required_user, :only => [:add_user]
+#  before_filter :require_no_user, :only => [:add_user]
+  skip_before_filter :require_user, :only => [:add_user]
   resource_controller   
 
   index.after do
