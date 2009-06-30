60c024828ba79efe769f2ed00f0393a4fe338ebd
wenju <liuwenju26@gmail.com>
2009-06-30 16:06:43 +0800

    fix bu

--- app/models/notify.rb
+++ app/models/notify.rb
@@ -16,6 +16,7 @@ class Notify < ActiveRecord::Base
   end                   
   
   def send_email
-    Mailer.send_later(:deliver_notify, self)
+   # Mailer.send_later(:deliver_notify, self)
+    Mailer.deliver_notify(self)
   end
 end
