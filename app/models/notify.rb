dc36a85a3060f61457a853c2b30050dff5fd030a
wenju <liuwenju26@gmail.com>
2009-06-30 16:15:19 +0800

    fix bug

--- app/models/notify.rb
+++ app/models/notify.rb
@@ -2,8 +2,8 @@ class Notify < ActiveRecord::Base
   belongs_to :notifiable, :polymorphic => true
   belongs_to :user
   
-  before_save :remove_blank_user
-  after_save :send_email
+  before_save  :remove_blank_user
+  after_create :send_email
   
   private 
   def remove_blank_user     #-------maybe be better?
@@ -16,7 +16,7 @@ class Notify < ActiveRecord::Base
   end                   
   
   def send_email
-   # Mailer.send_later(:deliver_notify, self)
-    Mailer.deliver_notify(self)
+  #  Mailer.deliver_notify(self)
+    Mailer.send_later(:deliver_notify, self)
   end
 end
