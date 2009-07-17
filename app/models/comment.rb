9bdfef877b3629c02e1bd66ca871ecbdc7efb275
wenju <liuwenju26@gmail.com>
2009-07-17 16:59:39 +0800

    fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,11 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true, :counter_cache => true
   belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
   
-  accepts_nested_attributes_for :assets                                                      
+  accepts_nested_attributes_for :assets        
+  
+  def code?
+    commentable_type == "Message" && commentable.category && commentable.category.name == "code"
+  end                                              
   
   
 end
